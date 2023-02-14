2dfbb8d4d2cadebffaaa861f433c1cf76ab251ea
Branden <44546325+BrandenEK@users.noreply.github.com>
2023-02-14 14:53:00 -0500

    Fix disable delay bug

--- SimpleTCP/SimpleTcpServer.cs
+++ SimpleTCP/SimpleTcpServer.cs
@@ -73,7 +73,7 @@ namespace SimpleTCP
             set
             {
                 for (int i = 0; i < _listeners.Count; i++)
-                    _listeners[i].DelayDisabled = true;
+                    _listeners[i].DelayDisabled = value;
             }
         }
 
